dd69c205af5307f7e6e2ec3aaf7ead6f1054a9da
nazmulmilon <milonsarker73@gmail.com>
2022-02-09 15:57:21 +0600

    category template and fetch data done

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns=[
     path('', views.home, name='home'),
+    path('category_list/', views.category_list, name='category_list'),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Category
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'index.html')
+
+# Category
+def category_list(request):
+    data=Category.objects.all().order_by('-id')
+    context={
+        'data': data,
+    }
+    return render(request, 'category_list.html', context)
+
+# Brands
